d6fc1649c40637a59c467b55921b50ca566cd70e
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2016-01-05 15:10:33 -0800

    Provision to have list and grid view on product listing page

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ $('.new-offers .fa-times-circle').click(function() {
 });
 
 // Script which keeps the menu bar on the top when the site scrolls down.
-$(function(){ 
+$(function(){
   if ($('.sticky').length == 0) {
     return;
   }
@@ -16,6 +16,37 @@ $(function(){
       $('.sticky').css({ position: 'fixed', top: 0 });
     } else {
       $('.sticky').css('position','static');
+    }
+  });
+});
+
+$(function() {
+
+  $(document).on('click', '.view-switch .grid-view', function(event) {
+    event.preventDefault();
+    $('.product-description').hide();
+
+    var list = $(".list-view");
+    var grid = $(".grid-view");
+
+    if ( list.hasClass('select')) {
+      $(".product-list-item").addClass('grid').children('.product-description').hide();
+      list.removeClass('select');
+      grid.addClass('select');
+    }
+  });
+
+$(document).on('click', '.view-switch .list-view', function(event) {
+    event.preventDefault();
+
+    $('.product-description').show();
+    var list = $(".list-view");
+    var grid = $(".grid-view");
+
+    if ( grid.hasClass('select')) {
+      $(".product-list-item").removeClass('grid');
+      grid.removeClass('select');
+      list.addClass('select');
     }
   });
 });
